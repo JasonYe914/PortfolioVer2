6a7bfaefdc40698b73956e0d84ef40eab93e592a
Jason <jason.ye914@gmail.com>
2026-03-22 05:18:49 -0400

    [Feature]: CSS for contact Page done

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -3,21 +3,46 @@ import {useState} from 'react';
 
 //TODO: Set up Contact page + backend
 export default function ContactPage(){
-    const [state, setState] = useState('Send')
+    const [state, setState] = useState('Send');
+    const [Name, setName] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Message, setMessage] = useState('');
 
+    //TODO: Implement Timer for sending, after 3s setState("sent")
     const handleClick = () => {
-        setState("Sending...")
+        let counter = 0;
+        if(counter <= 1000000){
+            setState("sending...");
+            counter++;
+        }
+        setState("sent!");
     }
 
     return(
         <div className='ContactPage-container'>
             <div className="Name-email">
                 <input
-
+                    type="text"
+                    placeholder="Name"
+                    value={Name}
+                    className="Name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={Email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="Email"
                 />
             </div>
             <div className="message">
-
+                <input
+                    type="text"
+                    value={Message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="Message"
+                />
             </div>
             <div className="send-btn">
                 <button onClick={handleClick}>{state}</button>
